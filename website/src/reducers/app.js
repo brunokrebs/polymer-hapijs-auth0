3f7a1625a98d1f0a6ebc8d909bce1d5c638df891
Bruno Krebs <bruno.krebs@auth0.com>
2018-07-11 18:25:19 -0300

    fixing app.js code style

--- website/src/reducers/app.js
+++ website/src/reducers/app.js
@@ -10,97 +10,66 @@ import {
   INIT_LOADING_END,
 } from '../actions/app.js';
 
-
 const initialState = {
-  user : null,
-  loadingdata : false,
-  loadinginitial : false,
-  items : [
-  ]
+  user: null,
+  loadingdata: false,
+  loadinginitial: false,
+  items: [],
 };
 
-
 export const reducer = (state, action) => {
-  if(!state) return initialState;
+  if (!state) return initialState;
+  let items;
 
   switch (action.type) {
-
     case INIT_LOADING_START:
-    {
       return Object.assign({}, state, {
-        loadinginitial : true,
+        loadinginitial: true,
       });
-    }
-    break;
 
     case INIT_LOADING_END:
-    {
       return Object.assign({}, state, {
-        items : action.items.map((elem) => { return {name : elem, loading: false} }),
-        loadinginitial : false,
+        items: action.items.map((elem) => {
+          return {name: elem, loading: false}
+        }),
+        loadinginitial: false,
       });
-    }
-    break;
 
     case ADD_ITEM_START:
-    {
-      let items = state.items.slice(0);
+      items = state.items.slice(0);
       items.unshift({
-        name : action.item,
+        name: action.item,
         loading: true
-      })
+      });
 
       return Object.assign({}, state, {
-        items : items,
-        loadingdata : true,
+        items: items,
+        loadingdata: true,
       });
-    }
-    break;
 
     case ADD_ITEM_FINISH:
-    {
-      let items = state.items.slice(0);
+      items = state.items.slice(0);
       delete items[0].loading;
-      return Object.assign({}, state, { items : items });
-    }
-    break;
+      return Object.assign({}, state, {items});
 
     case ADD_ITEM_FAILED:
-    {
-      let items = state.items.slice(0,-1).shift();
-      return Object.assign({}, state, { items : items });
-    }
-    break;
+      items = state.items.slice(0, -1).shift();
+      return Object.assign({}, state, {items});
 
     case DELETE_ITEM_START:
-    {
-      let items = state.items.slice(0);
+      items = state.items.slice(0);
       items[action.index].deleting = true;
-      return Object.assign({}, state, { items : items });
-    }
-    break;
+      return Object.assign({}, state, {items});
 
     case DELETE_ITEM_FINISH:
-    {
-      let items = state.items.slice(0);
-      items.splice(action.index,1);
-      return Object.assign({}, state, { items : items });
-    }
-    break;
+      items = state.items.slice(0);
+      items.splice(action.index, 1);
+      return Object.assign({}, state, {items});
 
     case USER_LOGIN:
-    {
-      return Object.assign({}, state, { user : action.user });
-    }
-    break;
+      return Object.assign({}, state, {user: action.user});
 
     case USER_LOGOUT:
-    {
       return initialState;
-    }
-    break;
-
-    default:
-
   }
-}
+};
