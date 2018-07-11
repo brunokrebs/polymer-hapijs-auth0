63bc37ad501e7edba7bd3837b0ac521538c19ba2
Bruno Krebs <bruno.krebs@auth0.com>
2018-07-11 18:19:57 -0300

    fixing webpack dev file indentation and code style;

--- website/webpack.dev.conf.js
+++ website/webpack.dev.conf.js
@@ -1,14 +1,14 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry : './src/components/my-app.js',
-    output : {
-        filename : 'main.js',
-    },
-    mode : 'development',
-    plugins : [
-    	new webpack.DefinePlugin({
-          "API_URL": '"'+'http://localhost:3000'+'"',
-    	})
-    ]
-  };
+  entry: './src/components/my-app.js',
+  output: {
+    filename: 'main.js',
+  },
+  mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin({
+      'API_URL': '"http://localhost:3000"',
+    })
+  ]
+};
